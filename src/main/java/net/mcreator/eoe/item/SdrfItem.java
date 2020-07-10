9626c74c511518e6ea0e6584a9a710bd03c0ca68
MartinPrograms <marti@DESKTOP-1HB477M>
2020-07-10 20:21:11 +0200

    jeas

---
+++ src/main/java/net/mcreator/eoe/item/SdrfItem.java
@@ -0,0 +1,55 @@
+
+package net.mcreator.eoe.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.MusicDiscItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+
+import net.mcreator.eoe.EoeModElements;
+
+import java.util.List;
+
+@EoeModElements.ModElement.Tag
+public class SdrfItem extends EoeModElements.ModElement {
+	@ObjectHolder("eoe:sdrf")
+	public static final Item block = null;
+	public SdrfItem(EoeModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new MusicDiscItemCustom());
+	}
+	public static class MusicDiscItemCustom extends MusicDiscItem {
+		public MusicDiscItemCustom() {
+			super(0, (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.anvil.break")),
+					new Item.Properties().group(ItemGroup.TRANSPORTATION).maxStackSize(1).rarity(Rarity.RARE));
+			setRegistryName("sdrf");
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("ah"));
+		}
+	}
+}
